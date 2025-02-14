aef1a0aec9c01085e18cdd904244e1299b0122d9
ameerbilal <99136205+ameerbilal@users.noreply.github.com>
2025-02-14 19:13:22 +0300

    Add files via upload

---
+++ web.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+}
+
+function toggleMenu() {
+    console.log("تم الضغط على زر القائمة!");
+    document.querySelector(".nav-links").classList.toggle("active");
+}
